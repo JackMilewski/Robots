05cfcc2f3f5b8499549a7a8792e27e9e0009ae15
JackMilewski <Jackmilewski49@gmail.com>
2025-05-16 13:02:19 -0400

    Update Robot.cs

--- Assets/Robot.cs
+++ Assets/Robot.cs
@@ -4,9 +4,6 @@ using System.Collections.Generic;
 
 public class Robot : MonoBehaviour {
 
-    public float endpoint1;
-    public float endpoint2;
-    
     [SerializeField] Rigidbody2D rb;
     [SerializeField] LayerMask ground;
     [SerializeField] LayerMask robotLayer;
@@ -18,7 +15,7 @@ public class Robot : MonoBehaviour {
     protected int health;
     protected int speed;
     protected int jump;
-    protected int weight;
+    protected float playerMultiplyer;
 
     private double aiMovementStaller;
     public bool grounded;
@@ -43,12 +40,12 @@ public class Robot : MonoBehaviour {
     {
          if (Input.GetKey(KeyCode.D)) { 
             Flip(true);
-            rb.AddForce(new Vector2 (speed, 0));
+            rb.AddForce(new Vector2 (speed * playerMultiplyer, 0));
          }
 
          if (Input.GetKey(KeyCode.A)) { 
             Flip(false);
-            rb.AddForce(new Vector2 (-speed, 0));
+            rb.AddForce(new Vector2 (-speed * playerMultiplyer, 0));
 
          }
 
@@ -58,54 +55,76 @@ public class Robot : MonoBehaviour {
         }
     }
 
-    public void baseAIControl() { 
+    public void AIRoam() { 
         
         aiMovementStaller += Time.deltaTime;
-        if (aiMovementStaller >= .05) {
+        if (aiMovementStaller >= .05)
+        {
 
-            endpoint1 = 27 - (transform.localScale.x * 2.5f);
-            endpoint2 = -27 + (transform.localScale.x * 2.5f);
+            float size = 53.5f;
+            float endpoint1 = 27 - (transform.localScale.x * 2.5f);
+            float endpoint2 = -27 + (transform.localScale.x * 2.5f);
             float momentum = 1;
+            bool shouldAIRoam = true;
 
-            Debug.DrawRay(transform.position, Vector2.down * 3, Color.blue, .05f);
-           if (grounded){ 
-                GameObject Platform = (Physics2D.Raycast(transform.position, Vector2.down, 3, ground).collider.gameObject);
-                endpoint1 = Platform.transform.localPosition.x + (Platform.transform.localScale.x);
-                endpoint2 = Platform.transform.localPosition.x - (Platform.transform.localScale.x);
-                momentum = ((Platform.transform.localScale.x - Math.Abs(transform.localPosition.x - ((endpoint1 + endpoint2) / 2))) / 3) + 1;
-                if (momentum > 2){ momentum = 2; };
+            Debug.DrawRay(transform.position, Vector2.down * 3.5f, Color.blue, .05f);
+            if (grounded)
+            {
+                GameObject Platform = (Physics2D.Raycast(transform.position, Vector2.down, 3.5f, ground).collider.gameObject);
+                size = Platform.transform.localScale.x;
+                endpoint1 = Platform.transform.localPosition.x + size;
+                endpoint2 = Platform.transform.localPosition.x - size;
+                shouldAIRoam = ShouldAIRoam(Platform);
             }
 
-           Vector2 side = new Vector2();
-           float rayXStart = 0;
-           if (isLeft) { 
-                side = Vector2.left; 
-                rayXStart = -1;
-           } else { 
-               side = Vector2.right;
-               rayXStart = 1;
-           }
+            momentum = Math.Abs((size - Math.Abs(transform.localPosition.x - ((endpoint1 + endpoint2) / 2))) / 3) + 1;
+            if (momentum > 2) { momentum = 2; }
 
-           rayXStart = transform.position.x + (rayXStart * 2.2f * transform.localScale.x);
-           Debug.DrawRay(new Vector2(rayXStart, transform.position.y), side * 3, Color.yellow, .05f);
-           RaycastHit2D hitScan = Physics2D.Raycast(new Vector2(rayXStart, transform.position.y), side, 3, robotLayer);
-           if(hitScan.collider != null && hitScan.collider.gameObject != Main.Hacked) Flip(isLeft);
+            if (shouldAIRoam)
+            {
+                Vector2 side = new Vector2();
+                float rayXStart = 0;
+                if (isLeft)
+                {
+                    side = Vector2.left;
+                    rayXStart = -1;
+                }
+                else
+                {
+                    side = Vector2.right;
+                    rayXStart = 1;
+                }
 
-           if (rb.transform.position.x >= endpoint1) Flip(false);
-           else if (rb.transform.position.x <= endpoint2) Flip(true);
-           if (rb.transform.position.x < endpoint1 && !isLeft) { rb.transform.position = new Vector2 (rb.transform.position.x + (.01f * speed * momentum), rb.transform.position.y); }
-           else if (rb.transform.position.x > endpoint2 && isLeft) { rb.transform.position = new Vector2 (rb.transform.position.x - (.01f * speed * momentum), rb.transform.position.y); } 
-           aiMovementStaller = 0;
-            
+                rayXStart = transform.position.x + (rayXStart * 2.2f * transform.localScale.x);
+                Debug.DrawRay(new Vector2(rayXStart, transform.position.y), side * 2, Color.yellow, .05f);
+                RaycastHit2D hitScan = Physics2D.Raycast(new Vector2(rayXStart, transform.position.y), side, 2, robotLayer);
+                if (hitScan.collider != null && hitScan.collider.gameObject != Main.Hacked) Flip(isLeft);
+
+                if (rb.transform.position.x >= endpoint1) Flip(false);
+                else if (rb.transform.position.x <= endpoint2) Flip(true);
+                if (rb.transform.position.x < endpoint1 && !isLeft) { rb.transform.position = new Vector2(rb.transform.position.x + (.01f * speed * momentum), rb.transform.position.y); }
+                else if (rb.transform.position.x > endpoint2 && isLeft) { rb.transform.position = new Vector2(rb.transform.position.x - (.01f * speed * momentum), rb.transform.position.y); }
+                aiMovementStaller = 0;
+
+            }
         }
     }
 
-    private void Flip (bool side) {
-         if (isLeft && side) {
+    public bool ShouldAIRoam(GameObject Platform)
+    {
+        int spaceFilled = 0;
+        for (int i; i < Platform.transform.localScale.x; )
+            Debug.DrawRay(transform.position + i, Vector2.down * 3.5f, Color.green, .05f);
+        return true
+    }
+
+
+    private void Flip (bool curr) {
+         if (isLeft && curr) {
             rb.transform.Rotate(new Vector3(0, 180, 0));
             isLeft = false; 
          }
-         else if (!isLeft && !side) {
+         else if (!isLeft && !curr) {
             rb.transform.Rotate(new Vector3(0, 180, 0));
             isLeft = true; 
          }
